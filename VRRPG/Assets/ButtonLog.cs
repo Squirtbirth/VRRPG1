467652948d9da5a25d619c4c17add90978923913
Squirtbirth <83897548+Squirtbirth@users.noreply.github.com>
2021-10-23 03:56:50 -0600

    trest

---
+++ VRRPG/Assets/ButtonLog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLog : MonoBehaviour
+{
+        public void Hello()
+            {
+                Debug.Log("I Clicked the Button");
+            }
+       
+}
